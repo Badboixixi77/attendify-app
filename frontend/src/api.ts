fc8f18aa62752e3ba3434a08330db960776532d5
Maxwell Okosun <okosunmaxwell9@gmail.com>
2026-07-13 03:48:42 -0400

    Prepare API url for production deployment

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+  // Use localhost in dev, but use the real domain in production
+  baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:3001/api' : '/api',
 });
 
 api.interceptors.request.use((config) => {
